bc60f89af3acaf8d3498b86565c7bbd1ad0f9e09
Saurabveer Singh <saurabveersingh@Saurabveers-MacBook-Air.local>
2024-10-06 00:01:52 +0100

    update footer

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -9,8 +9,9 @@
 const Footer = () => {
   return (
     <footer className="bg-black text-white p-5 w-100 d-flex justify-content-center align-items-center text-center position-fixed bottom-0">
-      Disclaimer: We do not sell any information. This form is only being shared to NUIG Students. To ensure security for yourself
-      and your peers, do not share the form response link to outsiders.
+      Disclaimer: We do not sell any information. This form is meant to be shared with NUIG Students. While using this service you
+      agree to publically share your contact information. To ensure security for yourself and your peers, avoid sharing the form
+      response link.
     </footer>
   )
 }
